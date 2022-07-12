968b1f5cb4b78b18c3545054858f88cc7e9c0c93
SamsAgency <samstore637@gmail.com>
2022-07-12 12:37:08 +0300

    completed course card items

--- src/layout/cards/courseitemcard.js
+++ src/layout/cards/courseitemcard.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Rating, Typography } from "@mui/material";
 import { styled } from "@mui/system";
+
+import { BsFillCalendarFill } from "react-icons/bs";
+import { FaLevelUpAlt, FaUsers } from "react-icons/fa";
 
 import Code from "../../assets/images/code.jpeg";
 import Author from "../../assets/images/african yut.jpg"
 
+const styledPriceFont = {
+	color: "rgba(221, 172, 12, 1)",
+	fontFamily: "'Mingzat', sans-serif",
+	fontWeight: "700",
+	fontSize: "20px"
+}
+
+const styledHeaderFont = {
+	color: "rgba(221, 172, 12, 1)",
+	fontFamily: "'Mingzat', sans-serif",
+	fontWeight: "700",
+	fontSize: "20px",
+	display: "block",
+	":firstLetter": {
+		textTransform: "capitalize"
+	}
+}
+
+const styledContentFont = {
+	
+}
+
 const StyledCourseItemCard = styled(Card)(({ theme }) => ({
 	width: "100%",
-	height: "100%"
+	height: "100%",
+	maxHeight: "540px"
 }));
 
 const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
@@ -16,16 +42,20 @@ const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
 }))
 
 const StyledAvatarBox = styled(Box)(({ theme }) => ({
-	height: "70px",
-	width: "70px",
 	borderRadius: "50%",
-	filter: "drop-shadow(10px 10px 10px rgba(0, 0, 0, 1) !important"
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	flexDirection: "column",
+	textAlign: "center",
+
 }))
 
 const styledImage = {
 	borderRadius: "50%",
-	width: "100%",
-	height: "100%",
+	height: "70px",
+	width: "70px",
+	alignSelf: "center"
 
 }
 
@@ -42,31 +72,58 @@ const StyledCardContent = styled(CardContent)(({ theme }) => ({
 	height: "30vh",
 	overflow: "elipsis",
 	marginBottom: "10px",
-	borderBottom: `1px solid grey`,
 	minHeight: "30vh",
 }))
 
-const StyledCardActions = styled(CardActions)(({ theme }) => ({
-	paddingTop : "10px",
-	paddingBottom : "10px",
-
-}))
-
-const StyledCardButton = styled(Button)(({ theme }) => ({
-	width: "100%",
-	height: "100%",
-	margin: "0px",
-	padding: "0px",
-}))
-
-const StyledCardActionAreaContent = styled(Box)(({ theme }) => ({
-	width: "100%",
-	height: "100%",
-	margin: "0px",
-	padding: "0px",
+const StyledLeanersContent = styled(Box)(({ theme }) => ({
 	display: "flex",
+	alignItems: "center",
 	justifyContent: "space-between",
-	alignItems: "center"
+	marginBottom: "20px"
+}))
+
+const styledLearnersIcon = {
+	marginRight: "10px"
+}
+
+const StyledLeanersArea = styled(Box)(({ theme }) => ({
+	display: "flex",
+	alignItems: "center",
+}))
+
+const StyledDurationArea = styled(Box)(({ theme }) => ({
+	display: "flex",
+	alignItems: "center",
+}))
+
+const StyledChipArea = styled(Box)(({ theme }) => ({
+	display: "flex",
+	alignItems: "center",
+}))
+
+const StyledParagraphSection = styled(Box)(({ theme }) => ({
+	height: "122px",
+	overflow: "hidden",
+	textOverflow: "ellipsis"
+}))
+
+const StyledRatingBox = styled(Box)(({ theme }) => ({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "space-between",
+	margin: "10px auto"
+}))
+
+const styledNumberStar = {
+	color: "rgba(221, 172, 12, 1)",
+	fontFamily: "'Mingzat', sans-serif",
+	fontWeight: "700",
+	fontSize: "20px"
+}
+
+const StyledReviewBox = styled(Box)(({ theme }) => ({
+	display: "flex",
+	alignItems: "center",
 }))
 
 const CourseItemCard = ({data}) => {
@@ -83,25 +140,66 @@ const CourseItemCard = ({data}) => {
 						alt="Instructor"
 						src={Author}
 						style={styledImage}/>
+					<Typography variant="h4">
+						Instructor's name
+					</Typography>
 				</StyledAvatarBox>
 
 				<StyledCardContent>
-					<Typography variant="h4" gutterBottom>
-						{data.title}
-					</Typography>
-					<Typography variant="body1" gutterBottom>
-						{data.description}
-					</Typography>
+
+					<StyledLeanersContent>
+						<StyledLeanersArea>
+							<FaUsers style={styledLearnersIcon}/>
+							<Typography variant="body2">
+								30 Enrolled
+							</Typography>
+						</StyledLeanersArea>
+
+						<StyledDurationArea>
+							<BsFillCalendarFill style={styledLearnersIcon}/>
+							<Typography variant="body2">
+								3 months Duration
+							</Typography>
+						</StyledDurationArea>
+
+						<StyledChipArea>
+							<FaLevelUpAlt style={styledLearnersIcon}/>
+							<Typography variant="body2">
+								{ data.level }
+							</Typography>
+						</StyledChipArea>
+					</StyledLeanersContent>
+
+					<StyledParagraphSection>
+						<Typography variant="h4" gutterBottom style={styledHeaderFont}>
+							{data.title}
+						</Typography>
+						<Typography variant="body1" gutterBottom>
+							{data.description}
+						</Typography>
+					</StyledParagraphSection>
+
+
+					<StyledRatingBox>
+							<StyledReviewBox>
+								<Typography variant="body1" sx={styledNumberStar}>
+									4.3
+								</Typography>
+								<Rating
+									name="Course rating"
+									value={4}
+									readOnly
+									sx={{marginLeft: "20px"}}
+									/>
+							</StyledReviewBox>
+
+							<Typography variant="body1" sx={styledPriceFont}>
+								40$
+							</Typography>
+						</StyledRatingBox>
+
 				</StyledCardContent>
 			</StyledCourseItemContentArea>
-
-			<StyledCardActions>
-				<StyledCardButton>
-					<StyledCardActionAreaContent>
-
-					</StyledCardActionAreaContent>
-				</StyledCardButton>
-			</StyledCardActions>
 		</StyledCourseItemCard>
 	)
 }
